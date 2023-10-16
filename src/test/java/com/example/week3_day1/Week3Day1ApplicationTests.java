fbd50c38f53007b685eb3d4d4d8e0a3ba86271ec
user <example@gmail.com>
2023-10-16 11:17:13 +0000

    Init the Project

---
+++ src/test/java/com/example/week3_day1/Week3Day1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.week3_day1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week3Day1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
